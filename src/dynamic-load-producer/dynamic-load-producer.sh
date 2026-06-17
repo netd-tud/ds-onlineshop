89d6f976a0d700ea2f1839cd0db5ea66548f4ea2
Florian <florian.zenkel@gmail.com>
2026-06-17 21:25:46 +0200

    fix(dynamic-load-producer): switch from fixed requests per minute to percentage of maximum request that can be handled

--- src/dynamic-load-producer/dynamic-load-producer.sh
+++ src/dynamic-load-producer/dynamic-load-producer.sh
@@ -4,20 +4,20 @@
 
 get_requests_per_minute() {
     local hour=$(date +%H)
-    local scaling_factor=10000
-    local base_rpm
+    local max_requests_per_minute=65000000 #rough maximum number of request that a single frontend-demo can handle without overloading
+    local percentage
     case $hour in
-        00|01|02|03|04|05) base_rpm=22 ;;    # 00:00 - 05:59
-        06|07) base_rpm=520 ;;               # 06:00 - 07:59
-        08|09) base_rpm=780 ;;               # 08:00 - 09:59
-        10|11) base_rpm=1000 ;;              # 10:00 - 11:59
-        12|13|14) base_rpm=1600 ;;           # 12:00 - 14:59
-        15|16) base_rpm=1150 ;;              # 15:00 - 16:59
-        17|18) base_rpm=800 ;;               # 17:00 - 18:59
-        19|20) base_rpm=400 ;;               # 19:00 - 20:59
-        21|22|23) base_rpm=350 ;;            # 21:00 - 23:59
+        00|01|02|03|04|05) percentage=10 ;;  # 00:00 - 05:59
+        06|07) percentage=40 ;;              # 06:00 - 07:59
+        08|09) percentage=50 ;;              # 08:00 - 09:59
+        10|11) percentage=90 ;;              # 10:00 - 11:59
+        12|13|14) percentage=110 ;;          # 12:00 - 14:59
+        15|16) percentage=90 ;;              # 15:00 - 16:59
+        17|18) percentage=75 ;;              # 17:00 - 18:59
+        19|20) percentage=40 ;;              # 19:00 - 20:59
+        21|22|23) percentage=20 ;;           # 21:00 - 23:59
     esac
-    echo "($base_rpm * $scaling_factor) / 1" | bc
+    echo $(( (percentage * max_requests_per_minute) / 100 ))
 }
 
 # Main loop
@@ -28,7 +28,7 @@ while true; do
     echo "load_producer_target_requests_per_minute $requests_per_minute" | \
           curl --data-binary @- http://prometheus:9091/metrics/job/dynamic-load-producer
 
-    curl -s -o /dev/null -X GET "http://frontend-demo:8080/heavyLoad?iters=$requests_per_minute"
+    curl -s -o /dev/null -X GET --connect-timeout 2 "http://frontend-demo:8080/heavyLoad?iters=$requests_per_minute"
 
     end_second=$(date +%s)
     elapsed=$(( end_second - start_second ))
