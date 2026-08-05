c61be225fcbe36286399f92269f461bbea93def1
Florian <florian.zenkel@gmail.com>
2026-08-05 10:19:17 +0200

    feat(flink): set correct option for bounded batch execution

--- kustomize/components/flink/sql/30_copurchase.sql
+++ kustomize/components/flink/sql/30_copurchase.sql
@@ -13,12 +13,13 @@ CREATE TABLE IF NOT EXISTS product_recommendations_sink (
   'sink.buffer-flush.interval' = '1s'
 );
 
+SET 'table.dynamic-table-options.enabled' = 'true';
 SET 'execution.runtime-mode' = 'BATCH';
 
 INSERT INTO product_recommendations_sink
 SELECT a.sku AS sku_a, b.sku AS sku_b, COUNT(*) AS co_purchase_count
-FROM product_events a
-JOIN product_events b
+FROM product_events /*+ OPTIONS('scan.bounded.mode'='latest-offset') */ a
+JOIN product_events /*+ OPTIONS('scan.bounded.mode'='latest-offset') */ b
   ON a.order_id = b.order_id
     AND a.sku < b.sku
     AND a.event_time BETWEEN b.event_time - INTERVAL '1' HOUR AND b.event_time + INTERVAL '1' HOUR
